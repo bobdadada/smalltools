f019832c7d2decd34cacad2860c0c62b23b7179c
baoxingyu <baoxingyubob@outlook.com>
2020-10-14 14:45:14 +0800

    添加中科大邮箱提醒功能

--- ustc_daily_report.py
+++ ustc_daily_report.py
@@ -8,9 +8,10 @@ import urllib
 import requests
 from bs4 import BeautifulSoup
 
+from ustc_email import notify_self
 from regexp import date as date_reg
 
-def main(usrname, password, sleep=True, start_delaym=2, interval_delaym=1):
+def main(usrname, password, sleep=True, start_delaym=2, interval_delaym=1, email_addr=None, email_passwd=None):
     print('每日健康上报！')
 
     if (not isinstance(usrname, str)) or (not isinstance(usrname, str)):
@@ -89,6 +90,15 @@ def main(usrname, password, sleep=True, start_delaym=2, interval_delaym=1):
 
     print('[*]完成每日健康上报！')
 
+    if isinstance(email_addr, str) and isinstance(email_passwd, str):
+        print('[+]发送提醒邮件')
+        try:
+            notify_self(email_addr, email_passwd, 
+                "%s 完成每日健康上报！"%(datetime.now().date()), subject="每日健康上报")
+            print('[-]发送提醒邮件成功')
+        except:
+            print('[!]发送提醒邮件失败')
+
 if __name__ == '__main__':
     import argparse
 
@@ -98,11 +108,13 @@ if __name__ == '__main__':
     parser.add_argument('--nosleep', action='store_false', help='快速响应服务器请求')
     parser.add_argument('--sdelay', nargs=1, type=int, default=2, help='开始延迟分钟数')
     parser.add_argument('--idelay', nargs=1, type=int, default=1, help='中间延迟分钟数')
+    parser.add_argument('--email_addr', type=str, help='中科大邮箱地址，用于发送邮件提醒')
+    parser.add_argument('--email_passwd', type=str, help='中科大邮箱密码，用于发送邮件提醒')
     args = parser.parse_args()
 
     try:
-        main(args.usrname, args.password, sleep=args.nosleep, start_delaym=args.sdelay, 
-            interval_delaym=args.idelay)
+        main(usrname=args.usrname, password=args.password, sleep=args.nosleep, start_delaym=args.sdelay, 
+            interval_delaym=args.idelay, email_addr=args.email_addr, email_passwd=args.email_passwd)
     except Exception as e:
         print(e)
         raise
