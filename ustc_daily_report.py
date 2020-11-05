c11594ce0f9501d75758a934586db7ddff5575c7
baoxingyu <baoxingyubob@outlook.com>
2020-11-05 16:44:30 +0800

    更新ustc_daily_report.py的结构，当前信息独立在default_daily_report.json中。

--- ustc_daily_report.py
+++ ustc_daily_report.py
@@ -10,10 +10,12 @@ import urllib
 import requests
 from bs4 import BeautifulSoup
 
+from utils.json_plus import load_json
 from utils.ustc_email import notify_self
 from utils.regexp import date as date_reg
 
-def main(usrname, password, sleep=True, start_delaym=2, interval_delaym=1, email_addr=None, email_passwd=None):
+
+def main(usrname, password, data_file, sleep=True, start_delaym=2, interval_delaym=1, email_addr=None, email_passwd=None):
     print('每日健康上报！')
 
     if (not isinstance(usrname, str)) or (not isinstance(usrname, str)):
@@ -59,26 +61,11 @@ def main(usrname, password, sleep=True, start_delaym=2, interval_delaym=1, email
         time.sleep(random.random()*interval_delaym*60)
 
     # 制造表单
-    data = {
-        '_token': html_login.findChild('input', {'name':'_token'}).attrs['value'],
-        'now_address': 1,  # 内地
-        'gps_now_address': '',
-        'now_province': 340000,  # 安徽省
-        'gps_province': '',
-        'now_city': 340100,  # 合肥市
-        'gps_city': '',
-        'now_detail': '',
-        'is_inschool': 2,  # 东区
-        'body_condition': 1,  # 正常
-        'body_condition_detail': '',
-        'now_status': 1,  # 正常在校园中
-        'now_status_detail': '',
-        'has_fever': 0,  # 无
-        'last_touch_sars': 0,  # 无
-        'last_touch_sars_date': '',
-        'last_touch_sars_detail': '',
-        'other_detail': ''
-    }
+    try:
+        data = load_json(data_file, js_comments=True)
+    except:
+        raise Exception('[!]导入个人报告信息失败，查看default_daily_report.json模板')
+    data.update({'_token': html_login.findChild('input', {'name':'_token'}).attrs['value']})
 
     # 上报
     print('[+]健康上报')
@@ -105,9 +92,10 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(description='中科大健康上报')
-    parser.add_argument('usrname', type=str, help='用户名称')
-    parser.add_argument('password', type=str, help='用户密码')
-    parser.add_argument('--nosleep', action='store_false', help='快速响应服务器请求')
+    parser.add_argument('usrname', type=str, help='学号')
+    parser.add_argument('password', type=str, help='密码')
+    parser.add_argument('file', type=str, help='记录当前信息的JSON文件')
+    parser.add_argument('--sleep', action='store_true', help='服务器短暂的延迟')
     parser.add_argument('--sdelay', nargs=1, type=int, default=2, help='开始延迟分钟数')
     parser.add_argument('--idelay', nargs=1, type=int, default=1, help='中间延迟分钟数')
     parser.add_argument('--email_addr', type=str, help='中科大邮箱地址，用于发送邮件提醒')
@@ -115,7 +103,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     try:
-        main(usrname=args.usrname, password=args.password, sleep=args.nosleep, start_delaym=args.sdelay, 
+        main(usrname=args.usrname, password=args.password, data_file=args.file, sleep=args.sleep, start_delaym=args.sdelay, 
             interval_delaym=args.idelay, email_addr=args.email_addr, email_passwd=args.email_passwd)
     except Exception as e:
         print(e)
