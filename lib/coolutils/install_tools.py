f0b61b72d36980b76d2d07840a1f26494d4cafd4
baoxingyu <baoxingyubob@outlook.com>
2020-11-28 14:40:25 +0800

    优化部分代码

--- lib/coolutils/install_tools.py
+++ lib/coolutils/install_tools.py
@@ -6,12 +6,19 @@ import os
 import shutil
 
 def install_all(srcdir, objdir, quiet=False, exception_ok=False):
+    except_filenames = []
     for filename in os.listdir(srcdir):
-        install(os.path.join(srcdir, filename), objdir, quiet, exception_ok)
+        try:
+            install(os.path.join(srcdir, filename), objdir, quiet, exception_ok)
+        except:
+            except_filenames.append(filename)
+    if except_filenames:
+        raise Exception('[!]更新软件%s失败'%(','.join(except_filenames)))
+
 
 def install(src, objdir, quiet=False, exception_ok=False):
     _, filename = os.path.split(src)
-    
+
     update_flag = False
 
     if filename in os.listdir(objdir):
@@ -22,7 +29,7 @@ def install(src, objdir, quiet=False, exception_ok=False):
             update_flag = True
     else:
         update_flag = True
-    
+
     if update_flag:
         if not quiet:
             print("[+]更新软件%s"%filename)
@@ -34,7 +41,7 @@ def install(src, objdir, quiet=False, exception_ok=False):
                 print("[!]更新软件%s失败"%filename)
             if not exception_ok:
                 raise
-        
+
         if not quiet:
             print("[-]更新软件%s成功"%filename)
     else:
