320094f8cd3851eb5ff70e27dceaa44f74a84529
baoxingyu <baoxingyubob@outlook.com>
2021-09-28 16:42:05 +0800

    整理，修改路径

--- bin/python(standalone).bat
+++ bin/python(standalone).bat
@@ -1,12 +1,11 @@
 @echo off
 
 set python=D:\ProgramData\Python\Python37
-set username=ASUS
 
 PATH = %python%;%PATH%
 PATH = %python%\Scripts;%PATH%
 
-cd C:\Users\%username%\Documents
+cd C:\Users\%USERNAME%\Documents
 
 cmd /K
 
